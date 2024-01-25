71147ffe52ead6fb5fd8eb64a3d48d7b5f1391ff
vishal <vishal88kh@gmail.com>
2024-01-26 00:07:38 +0530

    added all methods

--- src/main/java/org/anpr/ANPRApp.java
+++ src/main/java/org/anpr/ANPRApp.java
@@ -1,7 +1,12 @@
 package org.anpr;
 
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,11 +16,14 @@ import java.io.File;
 public class ANPRApp extends JFrame {
     private final JLabel imageLabel;
     private final JTextArea resultTextArea;
-    public ANPRApp( ){
-        setTitle("Automatic Number Plate Recognition : ");
-        setSize(800,600);
+
+    public ANPRApp() {
+        setTitle("Automatic Number Plate Recognition");
+        setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
+
+        // Set background color
         getContentPane().setBackground(new Color(255, 223, 186)); // Replace with your preferred RGB color
 
         imageLabel = new JLabel();
@@ -58,5 +66,52 @@ public class ANPRApp extends JFrame {
             e.printStackTrace();
             return null;
         }
+    }
+
+    private void displayImage(File imageFile) {
+        try {
+            BufferedImage image = processImage(imageFile);
+            ImageIcon imageIcon = new ImageIcon(image);
+            Image scaledImage = imageIcon.getImage().getScaledInstance(imageLabel.getWidth(), imageLabel.getHeight(), Image.SCALE_SMOOTH);
+            imageIcon = new ImageIcon(scaledImage);
+            imageLabel.setIcon(imageIcon);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void performOCR(File imageFile) {
+        try {
+            ITesseract tesseract = new Tesseract();
+            tesseract.setLanguage("eng");
+            tesseract.setDatapath("C:\\Program Files\\Tesseract-OCR\\tessdata");
+
+            String result = tesseract.doOCR(imageFile);
+            resultTextArea.setText("OCR Result:\n" + result);
+        } catch (TesseractException e) {
+            e.printStackTrace();
+            resultTextArea.setText("Error performing OCR.");
+        }
+    }
+
+    private void openImage() {
+        JFileChooser fileChooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Image files", "jpg", "jpeg", "png", "gif");
+        fileChooser.setFileFilter(filter);
+
+        int result = fileChooser.showOpenDialog(this);
+
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+
+            displayImage(selectedFile);
+            performOCR(selectedFile);
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new ANPRApp().setVisible(true);
+        });
     }
 }
